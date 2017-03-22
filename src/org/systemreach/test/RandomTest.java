200865451c8016a8146f0ecd1c7afbcae9c60a69
Gavin <Gavin@Flandre>
2017-03-21 21:51:17 -0500

    Added observers in RandomTest and hive2hive client on pom.xml

--- src/org/systemreach/test/RandomTest.java
+++ src/org/systemreach/test/RandomTest.java
@@ -4,8 +4,15 @@ import org.hive2hive.core.api.H2HNode;
 import org.hive2hive.core.api.configs.FileConfiguration;
 import org.hive2hive.core.api.configs.NetworkConfiguration;
 import org.hive2hive.core.api.interfaces.IFileConfiguration;
+import org.hive2hive.core.api.interfaces.IFileManager;
 import org.hive2hive.core.api.interfaces.IH2HNode;
 import org.hive2hive.core.api.interfaces.INetworkConfiguration;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.hive2hive.client.util.FileObserver;
+import org.hive2hive.client.util.FileObserverListener;
 
 public class RandomTest {
 
@@ -14,21 +21,37 @@ public class RandomTest {
 	}
 
 	private static void setupNode() {
-		//Two ways to create node.
-		//First step appears to be creating a file configuration.
-		//Second step is to establish the network.
-		//Lastly create the node.
+		// Two ways to create node.
+		// First step appears to be creating a file configuration.
+		// Second step is to establish the network.
+		// Lastly create the node.
 		IFileConfiguration fileConfiguration = FileConfiguration.createDefault();
-		//Create an uninitialized node.
+		// Create an uninitialized node.
 		IH2HNode node = H2HNode.createNode(fileConfiguration);
-		//Create a p2p network at first avaliable peer.
+		// Create a p2p network at first avaliable peer.
 		node.connect(NetworkConfiguration.createInitial());
 
-		//Configuring and setting up a new P2P network 
+		// Configuring and setting up a new P2P network
 		INetworkConfiguration netConfig = NetworkConfiguration.createInitial();
 		IFileConfiguration fileConfig = FileConfiguration.createDefault();
 
 		IH2HNode peerNode = H2HNode.createNode(fileConfig);
 		peerNode.connect(netConfig);
+
+		IFileManager fileManager = peerNode.getFileManager();
+
+		FileObserverListener listener = new FileObserverListener(fileManager);
+		
+		Path rootDirectory = Paths.get("./");
+
+		FileObserver observer = new FileObserver(rootDirectory.toFile());
+		observer.addFileObserverListener(listener);
+		try {
+			observer.start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
